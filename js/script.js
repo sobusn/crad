20ea95ddd43949f94313c944415891fd6ad48f6a
sobusn <107549346+sobusn@users.noreply.github.com>
2022-07-03 11:56:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1,
+          
+            
+        },
+        320:{
+            items:2,
+         
+        },
+        375:{
+            items:2,
+         
+        },
+        425:{
+            items:4,
+            
+        },
+        768:{
+            items:4,
+           
+        },
+        1024:{
+            items:5,
+            
+        },
+        1440:{
+            items:6,
+            
+        },
+    }
+})
